9228417516279a1670ac2434f60b657676ac6c72
Bhavnit Patel <bhavnitrulez291@hotmail.com>
2017-02-07 12:24:49 +0000

    first commit

---
+++ dist/out-tsc/environments/environment.prod.js
@@ -0,0 +1,4 @@
+export var environment = {
+    production: true
+};
+//# sourceMappingURL=D:/front-app/src/environments/environment.prod.js.map
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <user></user>
+    `
+})
+export class AppComponent {
+  title = 'app wosrks!';
+}
